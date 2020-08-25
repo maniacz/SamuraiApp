e9ad4b87f1cc5d0a206ab5a4ed71ddb3050bfe57
lukasz.kipka@gmail.com <lukasz.kipka@gmail.com>
2020-08-25 16:44:41 +0200

    Add project files.

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connStr = @"Data Source=.\SQLEXPRESS;Initial Catalog=SamuraiAppData;Integrated Security=True";
+            optionsBuilder.UseSqlServer(connStr);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });
+            modelBuilder.Entity<Horse>().ToTable("Horses");
+        }
+    }
+}
